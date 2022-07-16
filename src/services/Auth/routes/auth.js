305ec8039b853c90a6585b30c8b8d3114e5cb682
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-16 19:19:26 +0700

    [UPDATE] middleware auth and permission

--- src/services/Auth/routes/auth.js
+++ src/services/Auth/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/auth.controller');
 <<<<<<< HEAD
+<<<<<<< HEAD
 const authMiddleware = require('../../../middlewares/auth.middleware');
 const grantAccess = require('../../../middlewares/permission.middleware');
 const permission = require('../../../constants/permissions');
@@ -20,5 +21,19 @@ router.post('/login', controller.login);
 router.post('/register', controller.register);
 router.get('/refresh-token', controller.refreshToken);
 >>>>>>> 320f166 ([UPDATE] permission name)
+=======
+const authMiddleware = require('../../../middlewares/auth.middleware');
+const grantAccess = require('../../../middlewares/permission.middleware');
+const permission = require('../../../constants/permissions');
+router.post('/login', controller.login);
+router.post('/register', controller.register);
+router.get(
+  '/refresh-token',
+  authMiddleware,
+  grantAccess({ permission: permission.SEND.GIFT }),
+  controller.refreshToken
+);
+router.get('/logout', authMiddleware, controller.logout);
+>>>>>>> 7eea433 ([UPDATE] middleware auth and permission)
 
 module.exports = router;
