e4a08f8dd6cb579361b843c9c5b86773b7c2157e
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-25 20:07:59 +0700

    [MERGED] rebase from development

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,4 +8,6 @@ router.use('/media', require('../services/Media/routes'));
 router.use('/components', require('../services/Components/routes'));
 router.use('/auth', require('../services/Auth/routes'));
 router.use('/media', require('../services/media/routes'));
+router.use('/', require('../services/Auth/routes'));
+router.use('/components', require('../services/Components/routes'));
 module.exports = router;
