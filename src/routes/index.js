a98fd4413d3a1bfeec7cacd5d46ebb04bb5d099e
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-25 19:44:57 +0700

    [UPDATE] media service

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,4 +4,5 @@ router.use('/', require('../services/Auth/routes'));
 router.use('/media', require('../services/Media/routes'));
 router.use('/components', require('../services/Components/routes'));
 router.use('/auth', require('../services/Auth/routes'));
+router.use('/media', require('../services/media/routes'));
 module.exports = router;
