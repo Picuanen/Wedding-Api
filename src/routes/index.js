72ef61d4e283d32be7392126011144d3621c18e1
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-25 20:08:58 +0700

    [MERGED] rebase from development

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,4 +2,5 @@ const { Router } = require('express');
 const router = Router();
 router.use('/', require('../services/Auth/routes'));
 router.use('/components', require('../services/Components/routes'));
+router.use('/auth', require('../services/Auth/routes'));
 module.exports = router;
