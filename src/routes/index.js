a467fa21af7357b87d6b1e6c84df4ff844254b28
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-25 10:38:35 +0700

    [UPDATE] create templates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 router.use('/', require('../services/Auth/routes'));
+<<<<<<< HEAD
 router.use('/media', require('../services/Media/routes'));
+=======
+>>>>>>> 9989752 ([UPDATE] create templates)
 router.use('/components', require('../services/Components/routes'));
 module.exports = router;
--- src/services/Components/routes/index.js
+++ src/services/Components/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 // contr;
+<<<<<<< HEAD
+=======
+// router.use('/event', require('./event.route'));
+>>>>>>> 9989752 ([UPDATE] create templates)
 router.use('/templates', require('./template.route'));
 router.use('/layouts', require('./layout.route'));
 
