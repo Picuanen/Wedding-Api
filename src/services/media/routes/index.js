bd8027cc9721fd284faf50007b4d3bec13d9b1aa
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-25 19:44:57 +0700

    [UPDATE] media service

---
+++ src/services/media/routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/media.controller');
+
+router.get('/', controller.index);
+router.get('/get_media/:id', controller.getImage);
+// router.get('/:id', controller.show);
+router.post('/', controller.store);
+router.delete('/:id', controller.destroy);
+
+module.exports = router;
