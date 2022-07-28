004a9518bd65d5380b5aacaa18542db870413bf3
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-28 14:18:13 +0700

    [UPDATE] components

--- src/services/Components/routes/index.js
+++ src/services/Components/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 // contr;
-// router.use('/event', require('./event.route'));
 router.use('/templates', require('./template.route'));
 router.use('/layouts', require('./layout.route'));
 
