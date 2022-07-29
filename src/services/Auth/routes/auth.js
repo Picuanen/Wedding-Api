37068f36be3db614f4e43ea454c71a46f25f3fb3
Deweks0 <lutfiahjr1029f@gmail.com>
2022-07-29 13:46:30 +0700

    [UPDATE] asset management

--- src/services/Auth/routes/auth.js
+++ src/services/Auth/routes/auth.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/auth.controller');
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 const authMiddleware = require('../../../middlewares/auth.middleware');
 const grantAccess = require('../../../middlewares/permission.middleware');
 const permission = require('../../../constants/permissions');
@@ -17,40 +13,5 @@ router.get(
   controller.refreshToken
 );
 router.get('/logout', authMiddleware, controller.logout);
-=======
-=======
->>>>>>> 49b649c ([UPDATE] permission name)
-
-router.post('/login', controller.login);
-router.post('/register', controller.register);
-router.get('/refresh-token', controller.refreshToken);
-<<<<<<< HEAD
->>>>>>> 320f166 ([UPDATE] permission name)
-=======
-const authMiddleware = require('../../../middlewares/auth.middleware');
-const grantAccess = require('../../../middlewares/permission.middleware');
-const permission = require('../../../constants/permissions');
-router.post('/login', controller.login);
-router.post('/register', controller.register);
-=======
-const authMiddleware = require('../../../middlewares/auth.middleware');
-const grantAccess = require('../../../middlewares/permission.middleware');
-const permission = require('../../../constants/permissions');
-router.post('/login', controller.login);
-router.post('/register', controller.register);
->>>>>>> 545eea4 ([UPDATE] middleware auth and permission)
-router.get(
-  '/refresh-token',
-  authMiddleware,
-  grantAccess({ permission: permission.SEND.GIFT }),
-  controller.refreshToken
-);
-router.get('/logout', authMiddleware, controller.logout);
-<<<<<<< HEAD
->>>>>>> 7eea433 ([UPDATE] middleware auth and permission)
-=======
->>>>>>> 49b649c ([UPDATE] permission name)
-=======
->>>>>>> 545eea4 ([UPDATE] middleware auth and permission)
 
 module.exports = router;
